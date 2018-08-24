49b8d6b4f2c1aab8ac5ff0f3129b786392f768a0
vincenzo.siciliani <vincenzo.siciliani@nttdata.com>
2018-08-24 23:07:15 +0200

    Implement Singleton Class ThreadGestioneWifi

--- desktopApp/SnifferProbeRequestApp/SnifferProbeRequestApp/Program.cs
+++ desktopApp/SnifferProbeRequestApp/SnifferProbeRequestApp/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace SnifferProbeRequestApp
@@ -11,12 +12,19 @@ namespace SnifferProbeRequestApp
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
+
+        
+
         [STAThread]
         static void Main()
         {
+            ThreadGestioneWifi threadGestioneWifi = ThreadGestioneWifi.getIstance();
+            
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new frmMain());
+
+            threadGestioneWifi.stop();
         }
     }
 }
---
+++ desktopApp/SnifferProbeRequestApp/SnifferProbeRequestApp/ThreadGestioneWifi.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Threading;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Classe Singleton che wrappa il thread per la gestione del Wifi e dell'interfaccia verso le ESP
+
+namespace SnifferProbeRequestApp
+{
+    class ThreadGestioneWifi
+    {
+        private volatile bool stopThread;
+        private ThreadStart delegateThread;
+        private Thread thread;
+        private static ThreadGestioneWifi istance=null;
+
+        private ThreadGestioneWifi() {
+            stopThread = false;
+            start();
+        }
+
+        static public ThreadGestioneWifi getIstance() {
+            if (istance == null) {
+                istance = new ThreadGestioneWifi();
+            }
+            return istance;
+        }
+
+        public void start() {
+            delegateThread = new ThreadStart(elaboration);
+            thread = new Thread(delegateThread);
+            thread.Start();
+        }
+
+        public void stop() {
+            stopThread = true;
+            thread.Join();
+        }
+
+        private void elaboration() {
+            while (!stopThread)
+            {
+                Console.WriteLine("TEST THREAD");
+                Thread.Sleep(8000);
+            }
+        }
+
+    }
+}
