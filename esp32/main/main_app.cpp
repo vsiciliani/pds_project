0a96d9d27b679062fa2557c4b9a39a190c39d058
vsiciliani <siciliani.vincenzo@gmail.com>
2019-09-28 17:35:04 +0200

    Implemented usage of smart pointer

--- esp32/main/main_app.cpp
+++ esp32/main/main_app.cpp
@@ -14,6 +14,7 @@
 #include <esp_log.h>
 #include <string>
 #include <list>
+#include <memory>
 #include <thread>
 #include <mutex>
 #include <math.h>
@@ -35,19 +36,19 @@ void init_esp();
 static void wifi_sniffer_packet_handler(void *buff, wifi_promiscuous_pkt_type_t type);
 static esp_err_t event_handler(void* ctx, system_event_t* event);
 bool checkTimeoutThreadConnessionePc();
-void connectSocket();
+void connectSocket(std::shared_ptr<Socket>);
 void blinkLed();
-void syncClock();
+void syncClock(std::shared_ptr<Socket>);
 std::string createJSONArray(std::list<std::string>);
-bool sendMessage(std::string message);
-std::string receiveMessage();
+bool sendMessage(std::shared_ptr<Socket>, std::string message);
+std::string receiveMessage(std::shared_ptr<Socket>);
 
 //dichiarazioni variabili globali
 std::list<std::string> listaRecord;
 std::mutex m;
 std::condition_variable cvMinuto;
 time_t startWaitTime;
-Socket *s;
+//Socket *s;
 float memorySpace; //memoria inizialmente disponibile
 
 static EventGroupHandle_t wifi_event_group;
@@ -68,8 +69,9 @@ void app_main() {
 	do {
 		ESP_LOGI(tag, "Apertura socket con il server...");
 		//creo il socket
-		s = new Socket(SERVER_HOST, SERVER_PORT);
-		connectSocket();
+		std::shared_ptr<Socket> socket = std::make_shared<Socket>(SERVER_HOST, SERVER_PORT);
+		//s = new Socket(SERVER_HOST, SERVER_PORT);
+		connectSocket(socket);
 
 		//abilito la modalità di attività promiscua
 		ESP_ERROR_CHECK(esp_wifi_set_promiscuous(true));
@@ -77,7 +79,7 @@ void app_main() {
 
 		flag = true;
 		do {	
-			std::string message = receiveMessage();
+			std::string message = receiveMessage(socket);
 
 			if (message.compare("IDENTIFICA") == 0) {
 				//lancio il thread che si occupa di far lampeggiare il led
@@ -87,7 +89,7 @@ void app_main() {
 			}
 			else if (message.compare("SYNC_CLOCK") == 0) {
 				//effetto la sincronizzazione dei timestamp
-				syncClock();
+				syncClock(socket);
 			}
 			else if (message.compare("START_SEND") == 0) {
 				
@@ -108,7 +110,7 @@ void app_main() {
 				ESP_LOGI(tag, "Invio dati dei pacchetti al server");
 
 				//send dei dati verso il server
-				sendMessage(createJSONArray(listaRecord));
+				sendMessage(socket, createJSONArray(listaRecord));
 
 				//pulisco la lista di PacketInfo
 				listaRecord.clear();
@@ -122,7 +124,6 @@ void app_main() {
 		//abilito la modalità di attività promiscua
 		ESP_ERROR_CHECK(esp_wifi_set_promiscuous(false));
 		ESP_LOGI(tag, "Modalita schema promiscua disattivata");
-		delete s;
 		ESP_LOGI(tag, "Socket con il server chiuso");
 	} while (true);
 }
@@ -215,38 +216,38 @@ std::string createJSONArray(std::list<std::string>){
 }
 
 //funzione che esegue la connessione al socket se non e' già connesso
-void connectSocket(){
-	int res = s->connect();
+void connectSocket(std::shared_ptr<Socket> socket){
+	int res = socket->connect();
 
 	while (res < 0) {
 		ESP_LOGE(tag, "Connessione con il server fallita. Nuovo tentativo tra %d secondi...", TIMEOUT_RICONNESSIONE_SERVER);
 		//attende 10 secondi tra un tentativo di connessione e il successivo
 		sleep(TIMEOUT_RICONNESSIONE_SERVER);
-		res = s->connect();
+		res = socket->connect();
 	}
 	ESP_LOGI(tag, "Socket connesso");
 }
 
 //procedura per inviare un messaggio (stringa) al server
-bool sendMessage(std::string message){
+bool sendMessage(std::shared_ptr<Socket> socket, std::string message){
 	int numByteSent;
 	do {
-		numByteSent = s->send(message);
+		numByteSent = socket->send(message);
 	} while (numByteSent != message.length());
 	ESP_LOGI(tag, "Messaggio inviato al server con successo");
 	return true;
 }
 
 //procedura per ricevere un messaggio (stringa) dal socket
-std::string receiveMessage(){
+std::string receiveMessage(std::shared_ptr<Socket> socket){
 	//ripulisco il buffer di ricezione
-	std::string messaggio = s->receive();
+	std::string messaggio = socket->receive();
 	ESP_LOGI(tag, "Messaggio ricevuto: %s", messaggio.c_str());
 	return messaggio;
 }
 
 //procedura per sincronizzare il clock con il server
-void syncClock(){
+void syncClock(std::shared_ptr<Socket> socket){
 	long delay = 0;
 	long request_timestamp;
 	long reply_timestamp;
@@ -254,12 +255,12 @@ void syncClock(){
 
 	for (int i=0; i<4; i++){
 		time(&request_timestamp);
-		sendMessage("SYNC_CLOCK_START//n");
-		s->receiveRaw();
+		sendMessage(socket, "SYNC_CLOCK_START//n");
+		socket->receiveRaw();
 
 		received_timestamp = 0;
 		for (int j=0; j<8; j++){
-			received_timestamp += (s->buffer_ric[j] * pow(2, j*8));
+			received_timestamp += (socket->buffer_ric[j] * pow(2, j*8));
 		}
 		time(&reply_timestamp);
 
@@ -272,7 +273,7 @@ void syncClock(){
 	tv.tv_sec = received_timestamp + (delay/2);
 	tv.tv_usec = 0;
 	settimeofday(&tv, NULL);
-	sendMessage("SYNC_CLOCK_STOP//n");
+	sendMessage(socket, "SYNC_CLOCK_STOP//n");
 }
 
 //procedura che gestire il lampeggio del led quando viene richiesta dal server l'IDENTIFICAZIONE
