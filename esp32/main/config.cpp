1b6aad3668a30e32cb4ded5944e2294982624def
vsiciliani <siciliani.vincenzo@gmail.com>
2019-09-28 11:39:53 +0200

    Minor changes

--- esp32/main/config.cpp
+++ esp32/main/config.cpp
@@ -1,16 +1,10 @@
 //definizione costanti
 #define WIFI_SSID						"Vodafone-50650385"
 #define WIFI_PASS						"pe7dt3793ae9t7b"
-#define SERVER_HOST						"192.168.1.5"
+#define SERVER_HOST						"192.168.1.9"
 #define SERVER_PORT						5010
 #define INTERVALLO_CONNESSIONE_SERVER	20
 #define TIMEOUT_RICONNESSIONE_SERVER	10
-
-//std::string wifiSSID = "Vodafone-50650385";
- //std::string wifiPassword = "pe7dt3793ae9t7b";
-
- //std::string wifiSSID = "dlink-natale";
- //std::string wifiPassword = "h7onlgqmo8vcbgjr6qc3hg9v";
 
  //std::string wifiSSID = "APAndroid2";
  //std::string wifiPassword = "pippopluto";
