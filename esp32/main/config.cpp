60932b6fc91e606299b0324b4a14fefe4cb9c903
vsiciliani <siciliani.vincenzo@gmail.com>
2020-03-15 23:20:20 +0100

    Minor changes

--- esp32/main/config.cpp
+++ esp32/main/config.cpp
@@ -1,7 +1,7 @@
 //definizione costanti
-#define WIFI_SSID						"Vodafone-50650385"
-#define WIFI_PASS						"pe7dt3793ae9t7b"
-#define SERVER_HOST						"192.168.1.9"
+#define WIFI_SSID						"APAndroid2"
+#define WIFI_PASS						""
+#define SERVER_HOST						"192.168.43.3"
 #define SERVER_PORT						5010
 #define INTERVALLO_CONNESSIONE_SERVER	30
 #define TIMEOUT_RICONNESSIONE_SERVER	10
